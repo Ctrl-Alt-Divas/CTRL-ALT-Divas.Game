588c24a84a37994336aa1de33f54499778cb0b14
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2024-02-01 16:33:32 -0500

    set up indexjs file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-"hello"
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// init morgan
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// init body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// init cors
+const cors = require('cors');
+app.use(cors());
+
+// init db client
+const client = require('./db/client');
+client.connect();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Router: /api
+app.use('/api', require('./api'));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
