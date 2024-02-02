b0a83a85ca52e014dc2e4aa729d879f8a5d3146c
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2024-02-02 16:10:01 -0500

    merged with main

--- server/db/client.js
+++ server/db/client.js
@@ -1,6 +1,7 @@
 const { Client } = require('pg');
 require('dotenv').config({ path: './.env' });
 
-const client = new Client(process.env.DB_URL);
+// const client = new Client(process.env.DB_URL);
+const client = new Client('postgres://localhost:54321/cad');
 
 module.exports = client;
