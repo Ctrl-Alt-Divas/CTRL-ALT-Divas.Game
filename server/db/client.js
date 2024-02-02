213e7224960aeefa4e6eddd48ffee3e38e260777
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2024-02-02 16:14:34 -0500

    pushing dotenv and players

--- server/db/client.js
+++ server/db/client.js
@@ -1,7 +1,6 @@
 const { Client } = require('pg');
 require('dotenv').config({ path: './.env' });
 
-// const client = new Client(process.env.DB_URL);
-const client = new Client('postgres://localhost:54321/cad');
+const client = new Client(process.env.DB_URL);
 
 module.exports = client;
