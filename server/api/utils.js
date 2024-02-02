c04dad3c22eb2d13d728f723794b64f525cfd5de
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2024-02-02 11:54:35 -0500

    file structure updated

--- server/api/utils.js
+++ server/api/utils.js
@@ -1,11 +1,10 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../secrets');
 
 const authRequired = (req, res, next) => {
     const token = req.get('Authorization').split(' ')[1];
 
     try {
-        jwt.verify(token, JWT_SECRET)
+        jwt.verify(token, process.env.JWT_SECRET)
     } catch (error) {
         res.status(401).send({
             loggedIn: false,
