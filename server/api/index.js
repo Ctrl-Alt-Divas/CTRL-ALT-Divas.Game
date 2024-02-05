133c236847962d72f7f91b44d8f575a091da6d16
lcontr1 <contrerasliliana22@gmail.com>
2024-02-05 15:07:38 -0600

    fixed routing

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.use('/login', require('./auth'))
-router.use('/register', require('./auth'))
+router.get('/health', (req, res, next) => {
+    res.send("healthy route!")
+})
+
+router.use(express.json())
+router.use('/auth', require('./auth'))
+
 
 module.exports = router
