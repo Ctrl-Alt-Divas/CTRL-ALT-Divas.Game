8d171d8c6e7943951d9027f64a829d86cec01282
lcontr1 <contrerasliliana22@gmail.com>
2024-02-06 13:53:16 -0600

    added token to initial state and as credentials

--- client/src/app/slice.js
+++ client/src/app/slice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   player: sessionStorage.getItem('player') === null ? '' : JSON.parse(sessionStorage.getItem('player')),
   leaderboards: sessionStorage.getItem('leaderboards') === null ? '' : JSON.parse(sessionStorage.getItem('leaderboards')),
+  token: sessionStorage.getItem('token') === null ? '' : JSON.parse(sessionStorage.getItem('token'))
 };
 
 const slice = createSlice({
@@ -21,6 +22,17 @@ const slice = createSlice({
         state.leaderboards = payload.data;
         sessionStorage.setItem('leaderboards', JSON.stringify(state.leaderboards));
       }
+    },
+    setCredentials: (state, action) => {
+      const { player, token } = action.payload;
+      state.player = player;
+      state.token = token;
+      sessionStorage.setItem('player', JSON.stringify(state.player));
+    },
+    setToken: (state, action) => {
+      const { token } = action.payload;
+      state.token = token;
+      sessionStorage.setItem('token', JSON.stringify(state.token));
     },
   },
 });
