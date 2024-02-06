f61c99d50268ea51339990ba109f0d5c7b0de0b0
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2024-02-06 15:34:30 -0500

    Added Navbar logic and simple styling for visual

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,29 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Gameplay from "./components/Gameplay";
-import Account from "./components/Account";
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Register from './components/Register';
+import Gameplay from './components/Gameplay';
+import Account from './components/Account';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/gameplay" element={<Gameplay />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+    return (
+        <div className='h-full min-h-full m-0 p-0 flex flex-col justify-between'>
+            <Navbar />
+            <div className='mb-auto flex-1'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/account' element={<Account />} />
+                    <Route path='/gameplay' element={<Gameplay />} />
+                </Routes>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,42 @@
+import {Link, useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {updatePlayer} from '../app/slice';
+
 function Navbar() {
-  return <></>;
+    // token should be on the player object which is set on login
+    const token = useSelector((it) => it.state.player?.token);
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    // update player in state to be empty
+    function logout() {
+        dispatch(updatePlayer(''));
+        navigate('/');
+    }
+
+    return (
+        <div className='bg-purple-500 h-14 flex justify-between items-center pl-10 pr-10'>
+            <div className='flex gap-2 items-center'>
+                <img src='https://freepngtransparent.com/wp-content/uploads/2023/03/mario-png-62.png' width={30}></img>
+                <p className='text-xl text-fuchsia-500'>CTRL-ALT-DIVAS</p>
+            </div>
+            <div className='flex gap-10'>
+                <Link to='/'>Home</Link>
+                <Link to='/gameplay'>Game</Link>
+                {!token && <Link to='/register'>Register</Link>}
+                {!token && <Link to='/login'>Login</Link>}
+                {token && <Link to='/account'>Account</Link>}
+                {token && (
+                    <>
+                        <a onClick={() => logout()} to='/'>
+                            Logout
+                        </a>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
