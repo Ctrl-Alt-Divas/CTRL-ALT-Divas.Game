80ffe34f4989b22cc7d6e42f021e9b146be43471
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2024-02-06 14:15:00 -0500

    added some other stuff in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { store } from './app/store.js';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
