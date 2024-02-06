95e55f78b8dca0b09e9f4bbde78d1aaec79faa45
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2024-02-06 11:50:09 -0500

    completed storejs file

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { divasApi } from '../../api/divasApi';
+import slice from './slice';
 
 const store = configureStore({
-    
-})
+    reducer: {
+        state: createSlice,
+        [divasApi.reducerPath]: divasApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(divasApi.middleware),
+});
+
+export default store;
