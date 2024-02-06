44ef8764ab142b8c2cc2791635635da37cf46bc8
ElizabethLankford <elizabethlankford1@gmail.com>
2024-02-06 14:24:09 -0600

    fixed fetch error in api file

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,50 +1,59 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { useRegisterMutation } from "../../api/divasApi";
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { setCredentials } from "../app/slice";
 
 function Register() {
   const [player, setPlayer] = useState({
-    username: '',
-    password: ''
-  })
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const [register, { data, isSuccess }] = useRegisterMutation()
+    username: "",
+    password: "",
+  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [register, { data, isSuccess }] = useRegisterMutation();
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-    const { username, password } = player
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { username, password } = player;
     await register({
       username,
       password,
-    })
-    setPlayer({ username: '', password: '' })
-  }
+    }).unwrap();
+    setPlayer({ username: "", password: "" });
+  };
 
   useEffect(() => {
     if (isSuccess) {
-      dispatch(setCredentials(data))
-      navigate('/account');
+      dispatch(setCredentials(data));
+      navigate("/account");
     }
-  }, [data, dispatch, navigate, isSuccess]);
+  }, [isSuccess]);
 
   return (
-  <>
-   <form onSubmit={handleSubmit}>
-    <h2>Register</h2>
-    <label>
-      Username:
-      <input value={player.username} onChange={(e) => setPlayer({...player, username: e.target.value})} required />
-    </label>
-    <label>
-      password:
-      <input value={player.password} onChange={(e) => setPlayer({...player, password: e.target.value})} required />
-    </label>
-    <button>Register</button>
-   </form> 
-  </>)
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <label>
+          Username:
+          <input
+            value={player.username}
+            onChange={(e) => setPlayer({ ...player, username: e.target.value })}
+            required
+          />
+        </label>
+        <label>
+          password:
+          <input
+            value={player.password}
+            onChange={(e) => setPlayer({ ...player, password: e.target.value })}
+            required
+          />
+        </label>
+        <button>Register</button>
+      </form>
+    </>
+  );
 }
 
 export default Register;
