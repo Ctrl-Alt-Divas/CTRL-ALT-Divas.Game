37e704474fccac238779ddd12fc9aa434b454332
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2024-02-06 12:26:07 -0500

    changed update score to update player

--- client/src/app/slice.js
+++ client/src/app/slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
-  score: sessionStorage.getItem('score') === null ? '' : JSON.parse(sessionStorage.getItem('score')),
+  player: sessionStorage.getItem('player') === null ? '' : JSON.parse(sessionStorage.getItem('player')),
   leaderboards: sessionStorage.getItem('leaderboards') === null ? '' : JSON.parse(sessionStorage.getItem('leaderboards')),
 };
 
@@ -8,23 +8,23 @@ const slice = createSlice({
   name: 'slice',
   initialState,
   reducers: {
-    updateScore: (state, { payload }) => {
-      state.score = payload;
-      sessionStorage.setItem('score', JSON.stringify(state.score));
+    updatePlayer: (state, { payload }) => {
+      state.player = payload;
+      sessionStorage.setItem('player', JSON.stringify(state.player));
     },
     updateLeaderboard: (state, { payload }) => {
       if (state.leaderboards) {
-        state.leaderboards[payload.tag] = payload.data;
+        state.leaderboards = payload.data;
         sessionStorage.setItem('leaderboards', JSON.stringify(state.leaderboards));
       } else {
         state.leaderboards = {};
-        state.leaderboards[payload.tag] = payload.data;
+        state.leaderboards = payload.data;
         sessionStorage.setItem('leaderboards', JSON.stringify(state.leaderboards));
       }
     },
   },
 });
 
-export const { updateScore, updateLeaderboard } = slice.actions;
+export const { updatePlayer, updateLeaderboard } = slice.actions;
 
 export default slice.reducer;
