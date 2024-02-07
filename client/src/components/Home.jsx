7ccb4fa2e9aff23850b174d32cc6eede348d0ef7
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2024-02-07 11:44:41 -0500

    working on Landingpage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,22 @@
 function Home() {
-  return <></>;
+    return (
+        <>
+            <div className='flex flex-col'>
+                <div className='flex flex-col basis-3/4'>
+                    <img></img>
+                </div>
+                <div>Leaderboard</div>
+            </div>
+            <div className='flex flex-col'>
+                <div>Lani</div>
+                <div>Lilypad</div>
+                <div>Fancy</div>
+                <div>Eli</div>
+            </div>
+            <div>How to Play</div>
+            <div>Carousel for Videos</div>
+        </>
+    );
 }
 
 export default Home;
