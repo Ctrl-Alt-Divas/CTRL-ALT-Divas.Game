b316ebb654ab5cd4b148d5564492dc40832ed989
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2024-02-07 16:15:34 -0500

    Got Leaderboard to render and did some css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
     useEffect(() => {
         const fetchData = async () => {
             const leaderboard = await getLeaderboard();
-            dispatch(updateLeaderboard(leaderboard.data));
+            dispatch(updateLeaderboard(leaderboard));
             setLeaderboard(leaderboard.data);
 
             const characters = await getCharacters();
@@ -31,14 +31,18 @@ function Home() {
         for (const player of leaderboard) {
             if (counter === 10) break;
             rows.push(
-                <div className='flex gap-9 items-center'>
-                    <p className='text-xl text-pink-400'>{leaderboard.indexOf(player) + 1}</p>
-                    <img
-                        className='rounded-full h-[50px] border-2'
-                        src={new URL(`../assets/images/profile/${player.image}`, import.meta.url).href}
-                        width={50}
-                    />
-                    <p className='text-xl text-pink-400'>{player.username}</p>
+                <div className='flex gap-10 items-center justify-between'>
+                    <div className='flex gap-10 items-center'>
+                        <p className='text-xl text-pink-400'>{leaderboard.indexOf(player) + 1}</p>
+                        <div className='flex items-center gap-2'>
+                            <img
+                                className='rounded-full h-[50px] border-2'
+                                src={new URL(`../assets/images/profile/${player.image}`, import.meta.url).href}
+                                width={50}
+                            />
+                            <p className='text-xl text-pink-400'>{player.username}</p>
+                        </div>
+                    </div>
                     <p className='text-xl text-cyan-400'>{player.score}</p>
                 </div>
             );
@@ -52,11 +56,11 @@ function Home() {
         for (const character of characters) {
             rows.push(
                 <Link to={`/characters/${character.id}`}>
-                    <div className='bg-purple-600 w-[200px] flex justify-between flex-col p-5 rounded-md'>
+                    <div className='bg-fuchsia-800 w-[250px] flex justify-between flex-col p-5 rounded-md'>
                         <p>{character.name}</p>
                         <img
                             src='https://freepngtransparent.com/wp-content/uploads/2023/03/mario-png-62.png'
-                            width={200}
+                            width={250}
                         />
                         {/* Maybe add our short description to the seedData? */}
                         <p>Short description</p>
@@ -69,37 +73,42 @@ function Home() {
 
     return (
         <>
-            <div className='flex w-full'>
-                <div className='flex flex-col hero-bg h-[600px] basis-3/4 items-center justify-end'>
-                    <Link to='/gameplay' className='bg-purple-500 w-24 p-2 rounded-md text-lg mb-10'>
-                        Play Now
-                    </Link>
+            <div className='flex flex-col gap-20'>
+                <div className='flex w-full border-2 border-cyan-400'>
+                    <div className='flex flex-col hero-bg h-[700px] basis-3/4 items-center justify-end'>
+                        <Link to='/gameplay' className='bg-purple-500 w-24 p-2 rounded-md text-lg mb-10'>
+                            Play Now
+                        </Link>
+                    </div>
+                    <div className='flex flex-col basis-1/4 items-center border-2 border-cyan-400'>
+                        <div className='text-pink-500 text-2xl mb-5 mt-2'>Leaderboards</div>
+                        {leaderboard && leaderboard.length > 0 && (
+                            <div className='flex flex-col gap-3'>{createLeaderboard()}</div>
+                        )}
+                    </div>
                 </div>
-                <div className='flex flex-col basis-1/4 items-center border-2'>
-                    <div className='text-pink-500 text-2xl mb-5 mt-2'>Leaderboards</div>
-                    {leaderboard && leaderboard.length > 0 && (
-                        <div className='flex flex-col gap-3'>{createLeaderboard()}</div>
-                    )}
+
+                <hr className='border-0 bg-indigo-800 rounded w-2/3 h-1 mx-auto' />
+
+                <div className='flex flex-col gap-10 items-center'>
+                    <h2 className='text-purple-500 text-3xl'>Characters</h2>
+                    {characters && characters.length > 0 && <div className='flex gap-20'>{createCharacters()}</div>}
                 </div>
-            </div>
-            <div className='flex flex-col mb-4 gap-10 p-10 items-center'>
-                <h2 className='text-purple-500 text-3xl'>Characters</h2>
-                <div className='flex gap-10'>
-                  {createCharacters()}
+                <hr className='border-0 bg-indigo-800 rounded w-2/3 h-1 mx-auto' />
+                <div className='bg-pink-600 border-2 border-cyan-400 p-5 ml-20 mr-20 mb-20'>
+                    <h2 className='text-3xl mb-5'>How to Play</h2>
+                    <p className='text-xl'>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting, remaining essentially unchanged. It was
+                        popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                        and more recently with desktop publishing software like Aldus PageMaker including versions of
+                        Lorem Ipsum.
+                    </p>
                 </div>
+                {/* <div>Carousel for Videos</div> */}
             </div>
-            {/* <div className='bg-pink-600 border-2'>
-                <h2>How to Play</h2>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </div>
-            <div>Carousel for Videos</div> */}
         </>
     );
 }
