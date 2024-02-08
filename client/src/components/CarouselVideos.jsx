4115b85014a35449e20fc2e31da0eebafaf2ffc3
ElizabethLankford <elizabethlankford1@gmail.com>
2024-02-08 10:01:09 -0600

    set up video carousel

---
+++ client/src/components/CarouselVideos.jsx
@@ -0,0 +1,46 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel from "react-bootstrap/Carousel";
+
+function CarouselVideos() {
+  return (
+    <div className="carousel-container">
+      <Carousel slide={false}>
+        <Carousel.Item>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/FEARARPNS6I?si=z2IOUb2S5rLWjPnK"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            origin="true"
+          ></iframe>
+        </Carousel.Item>
+        <Carousel.Item>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/-39kuBXyKWo?si=iYu8VS7y0dRFmu5A"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            origin="true"
+          ></iframe>
+        </Carousel.Item>
+        <Carousel.Item>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/pu76c3HCKg4?si=cvuuG1Pmb47tLr8K"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            origin="true"
+          ></iframe>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default CarouselVideos;
