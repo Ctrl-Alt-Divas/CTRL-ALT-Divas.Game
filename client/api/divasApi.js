7cf8191cc160dc901035712d8970169026cf0c07
FancyStrittholt <fancypants@DESKTOP-TSFMQAQ>
2024-02-22 11:59:47 -0500

    baseurl

--- client/api/divasApi.js
+++ client/api/divasApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const divasApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8080/api',
+    baseUrl: 'https://ctrl-alt-divas-api.onrender.com/api',
   }),
   endpoints: (builder) => ({
     register: builder.mutation({
