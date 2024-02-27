d564b0c5baebf23ac3a3e0786a510d12aac74e31
lcontr1 <contrerasliliana22@gmail.com>
2024-02-26 23:21:00 -0600

    toggle menu implemented and responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,12 +7,20 @@ import {BiSolidUserAccount} from 'react-icons/bi';
 import {IoMdLogOut} from 'react-icons/io';
 import {IoMdLogIn} from 'react-icons/io';
 import {IoHome} from 'react-icons/io5';
+import { useState } from 'react';
+import { CiMenuBurger } from "react-icons/ci";
 
 function Navbar() {
+    
     // token should be on the player object which is set on login
     const token = useSelector((it) => it.state?.token);
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     const dispatch = useDispatch();
+
+    function toggleMenu() {
+        setIsMenuOpen(!isMenuOpen)
+    }
 
     // update player in state to be empty
     async function logout() {
@@ -21,60 +29,108 @@ function Navbar() {
     }
 
     return (
-        <div className='bg-indigo-950 h-14 flex justify-between items-center pl-10 pr-10'>
-            <Link to='/'>
-                <div className='flex gap-2 items-center text-purple-300 hover:text-cyan-500 p-1'>
-                    <img src={new URL(`../assets/images/icon.png`, import.meta.url).href} width={40} />
-                    <p className='text-2xl text-fuchsia-500'>CTRL-ALT-DIVAS</p>
-                </div>
-            </Link>
-            <div className='flex gap-10'>
-                <Link to='/' className='text-xl text-purple-300 hover:text-cyan-500'>
-                    <div className='flex gap-2 items-center'>
-                        <IoHome className='text-fuchsia-500 size-6' />
-                        Home
-                    </div>
-                </Link>
-                {token && (
-                    <Link to='/character-select' className='text-xl text-purple-300 hover:text-cyan-500'>
-                        <div className='flex gap-2 items-center'>
-                            <SlGameController className='text-blue-600 size-6' />
-                            Game
-                        </div>
+              
+
+    <header className='bg-gray-900'>
+        <div className='flex items-center justify-between px-8 py-2'>
+
+           
+            <div >
+                    <Link to='/' className='flex items-center text-cyan-600 hover:text-purple-600'>
+                        <img className='h-12' src={new URL(`../assets/images/icon.png`, import.meta.url).href} alt='Arcade Machine'/>
+                        CTRL-ALT-DIVAS
                     </Link>
-                )}
-                <Link to='/leaderboard' className='text-xl text-purple-300 hover:text-cyan-500'>
-                    <div className='flex gap-2 items-center'>
-                        <GiTrophyCup className='text-[#dd784b] size-6' />
-                        Leaderboard
-                    </div>
-                </Link>
-                {!token && (
-                    <Link to='/login' className='text-xl text-purple-300 hover:text-cyan-500'>
-                        <div className='flex gap-2 items-center'>
-                            <IoMdLogIn className='text-purple-600 size-6' />
-                            Login
-                        </div>
-                    </Link>
-                )}
-                {token && (
-                    <Link to='/account' className='text-xl text-purple-300 hover:text-cyan-500'>
-                        <div className='flex gap-2 items-center'>
-                            <BiSolidUserAccount className='text-green-600 size-6' />
-                            Account
-                        </div>
-                    </Link>
-                )}
-                {token && (
-                    <a onClick={() => logout()} href='/' className='text-xl text-purple-300 hover:text-cyan-500'>
-                        <div className='flex gap-2 items-center'>
-                            <IoMdLogOut className='text-purple-600 size-6' />
-                            Logout
-                        </div>
-                    </a>
-                )}
             </div>
+            <div >
+                <button type='button' className='text-purple-500 hover:text-cyan-600 focus:text-cyan-600 lg:hidden' onClick={toggleMenu}>
+                    <CiMenuBurger className='h-6 w-6 fill-current'/>
+                    {isMenuOpen ? 'Close' : 'Menu'}
+                </button>
+            </div>
+        <div className='hidden lg:flex'>
+        <Link to='/' className='flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'><IoHome />Home
+            </Link>
+            
+            <Link to='/leaderboard' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                <GiTrophyCup />
+                Leaderboard
+            </Link>
+            {!token && (
+                    <Link to='/login' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                        
+                            <IoMdLogIn />
+                            Login
+                        
+            </Link>
+            )}
+            {token && (
+            <Link to='/character-select' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                <SlGameController />
+                Game
+            </Link>
+            )}
+            {token && (
+            <Link to='/account' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                        
+                            <BiSolidUserAccount />
+                            Account
+                        
+            </Link>
+            )}
+            {token && (
+            <a onClick={() => logout()} href='/' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                        
+                            <IoMdLogOut />
+                            Logout
+                        
+            </a>
+            )}
         </div>
+        </div>
+        {isMenuOpen && (
+        <div className='lg:hidden px-6 pt-2 pb-4'>
+            <Link to='/' className='flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'><IoHome />Home
+            </Link>
+            
+            <Link to='/leaderboard' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                <GiTrophyCup />
+                Leaderboard
+            </Link>
+            {!token && (
+                    <Link to='/login' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                        
+                            <IoMdLogIn />
+                            Login
+                        
+            </Link>
+            )}
+            {token && (
+            <Link to='/character-select' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                <SlGameController />
+                Game
+            </Link>
+            )}
+            {token && (
+            <Link to='/account' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                        
+                            <BiSolidUserAccount />
+                            Account
+                        
+            </Link>
+            )}
+            {token && (
+            <a onClick={() => logout()} href='/' className='mt-1 flex items-center px-2 text-purple-600 rounded hover:bg-cyan-900 font-semibold'>
+                        
+                            <IoMdLogOut />
+                            Logout
+                        
+            </a>
+            )}
+          
+        </div>
+        )}
+
+    </header>
     );
 }
 
