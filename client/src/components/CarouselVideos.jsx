227ef4c5c414ccf9a359669c0b8e43ba2c919b0f
ElizabethLankford <elizabethlankford1@gmail.com>
2024-02-28 16:08:58 -0600

    added pause to bootstrap

--- client/src/components/CarouselVideos.jsx
+++ client/src/components/CarouselVideos.jsx
@@ -1,69 +1,67 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Carousel from "react-bootstrap/Carousel";
 
-
 function CarouselVideos() {
   return (
     <div className="carousel-contianer">
-      <Carousel slide={false}>
+      <Carousel className="carousel" slide={false} data-pause="mouseenter">
         <Carousel.Item>
-      <div className="embed-responsive embed-responsive-16by9">
-          <iframe
-            className="embed-responsive-item"
-            width="560"
-            height="315"
-            src="https://youtube.com/embed/eyu9604Envk?si=otuascUOyvUX1gr1"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            origin="true"
-          ></iframe>
-      </div>
-        </Carousel.Item>
-        <Carousel.Item>
-        <div className="embed-responsive embed-responsive-16by9">
-          <iframe
-            className="embed-responsive-item"
-            width="560"
-            height="315"
-            src="https://youtube.com/embed/k-mOyjjWTfc?si=SK_0VzRjTcMHPmVa"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            origin="true"
-          ></iframe>
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe
+              className="embed-responsive-item"
+              width="560"
+              height="315"
+              src="https://youtube.com/embed/eyu9604Envk?si=otuascUOyvUX1gr1"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+              origin="true"
+            ></iframe>
           </div>
         </Carousel.Item>
         <Carousel.Item>
-        <div className="embed-responsive embed-responsive-16by9">
-          <iframe
-            className="embed-responsive-item"
-            width="560"
-            height="315"
-            src="https://youtube.com/embed/vtcDrXM-ChQ?si=tT2vREy7B_WXYbGg"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            origin="true"
-          ></iframe>
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe
+              className="embed-responsive-item"
+              width="560"
+              height="315"
+              src="https://youtube.com/embed/k-mOyjjWTfc?si=SK_0VzRjTcMHPmVa"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+              origin="true"
+            ></iframe>
           </div>
         </Carousel.Item>
         <Carousel.Item>
-        <div className="embed-responsive embed-responsive-16by9">
-          <iframe
-            className="embed-responsive-item"
-            width="560"
-            height="315"
-            src="https://youtube.com/embed/OmwCdyokZ5k?si=Juwovxb2TYrVFhyH"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            origin="true"
-          ></iframe>
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe
+              className="embed-responsive-item"
+              width="560"
+              height="315"
+              src="https://youtube.com/embed/vtcDrXM-ChQ?si=tT2vREy7B_WXYbGg"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+              origin="true"
+            ></iframe>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe
+              className="embed-responsive-item"
+              width="560"
+              height="315"
+              src="https://youtube.com/embed/OmwCdyokZ5k?si=Juwovxb2TYrVFhyH"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+              origin="true"
+            ></iframe>
           </div>
         </Carousel.Item>
       </Carousel>
-   
     </div>
   );
 }
