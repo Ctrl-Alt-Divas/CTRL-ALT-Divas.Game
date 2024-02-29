cd368598a5797388201c20afd24ad7688cdf8791
ElizabethLankford <elizabethlankford1@gmail.com>
2024-02-29 11:44:17 -0600

    edited links for mute

--- client/src/components/CarouselVideos.jsx
+++ client/src/components/CarouselVideos.jsx
@@ -3,7 +3,7 @@ import Carousel from "react-bootstrap/Carousel";
 
 function CarouselVideos() {
   return (
-    <div className="carousel-contianer">
+    <div>
       <Carousel className="carousel" slide={false} data-pause="mouseenter">
         <Carousel.Item>
           <div className="embed-responsive embed-responsive-16by9">
@@ -11,7 +11,7 @@ function CarouselVideos() {
               className="embed-responsive-item"
               width="560"
               height="315"
-              src="https://youtube.com/embed/eyu9604Envk?si=otuascUOyvUX1gr1"
+              src="https://youtube.com/embed/eyu9604Envk?si=otuascUOyvUX1gr1&mute=1"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
@@ -25,7 +25,7 @@ function CarouselVideos() {
               className="embed-responsive-item"
               width="560"
               height="315"
-              src="https://youtube.com/embed/k-mOyjjWTfc?si=SK_0VzRjTcMHPmVa"
+              src="https://youtube.com/embed/k-mOyjjWTfc?si=SK_0VzRjTcMHPmVa&mute=1"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
@@ -39,7 +39,7 @@ function CarouselVideos() {
               className="embed-responsive-item"
               width="560"
               height="315"
-              src="https://youtube.com/embed/vtcDrXM-ChQ?si=tT2vREy7B_WXYbGg"
+              src="https://youtube.com/embed/vtcDrXM-ChQ?si=tT2vREy7B_WXYbGg&mute=1"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
@@ -53,7 +53,7 @@ function CarouselVideos() {
               className="embed-responsive-item"
               width="560"
               height="315"
-              src="https://youtube.com/embed/OmwCdyokZ5k?si=Juwovxb2TYrVFhyH"
+              src="https://youtube.com/embed/OmwCdyokZ5k?si=Juwovxb2TYrVFhyH&mute=1"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
